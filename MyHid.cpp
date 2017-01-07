85a734113354b86dd36e0bcfafca5587ba797b82
kpli <781171312@qq.com>
2017-01-07 16:26:35 +0800

    modified MyHid

--- MyHid.cpp
+++ MyHid.cpp
@@ -1,8 +1,38 @@
 ﻿#include "MyHid.h"
-#include <qdebug.h>
+#include <iostream>
 
-bool MyHid::SearchOpen()
+bool MyHid::isExit_ = true;
+
+MyHid::MyHid()
 {
+	isOpen_ = false;
+	isInit_ = false;
+	vid_ = 0x0485;
+	pid_ = 0x00a8;
+	for (int i = 0; i < HANDLE_COUNT; ++i)
+	{
+		handle_[i] = nullptr;
+	}
+}
+
+MyHid::MyHid(unsigned short vid, unsigned short pid)
+	:vid_(vid),
+	pid_(pid)
+{
+	isOpen_ = false;
+	isInit_ = false;
+	for (int i = 0; i < HANDLE_COUNT; ++i)
+	{
+		handle_[i] = nullptr;
+	}
+}
+
+bool MyHid::Open()
+{
+	struct hid_device_info *devs_;
+	struct hid_device_info *cur_dev_;
+	devs_ = cur_dev_ = nullptr;
+
 	if (!isInit_)
 	{
 		if (hid_init())
@@ -11,41 +41,119 @@ bool MyHid::SearchOpen()
 	}
 	devs_ = hid_enumerate(vid_, pid_);
 	cur_dev_ = devs_;
-	
 	for (int i = 0; cur_dev_ != nullptr; ++i, cur_dev_ = cur_dev_->next)
 	{
 		handle_[i] = hid_open_path(cur_dev_->path);
-		qDebug() << cur_dev_->path;
-		if (!handle_) {
-			qDebug() << "unable to open device";
-			return false;
-		}
+		if (!handle_[i]) return false;
 	}
+	if (handle_[0] != nullptr || handle_[1] != nullptr)
+		isOpen_ = true;
 	hid_free_enumeration(devs_);
 	return true;
 }
 
-bool MyHid::SendCmd1()
+bool MyHid::SendCmd(uint8_t* buf, int len)
 {
-	int res = -1;
-	uint8_t buf[65];
-	buf[0] = 0x01;
-	buf[1] = 0x11;
-	buf[2] = 0x11;
-	buf[3] = 0x11;
-	
-	for (int i = 0; i < 5; ++i)
+	int isSuccess = false;
+
+	if (!isOpen_)
+		if (!Open())
+			return false;
+	for (int i = 0; i < HANDLE_COUNT; ++i)
 	{
 		if (handle_[i])
 		{
-			qDebug() << "[send cmd] : send1" << endl;
-			int res = hid_write(handle_[i], buf, 33);
-			qDebug() << "[send cmd] : send2 res = " << res;
+			int res = hid_write(handle_[i], buf, len);
 			if (res <= 0)
 				continue;
 			else
-				return true;
+				isSuccess = true;
+		}
+		else
+		{
+			break;
 		}
 	}
+	if (isSuccess)
+		return true;
 	return false;
+}
+
+
+void MyHid::Close()
+{
+	isInit_ = false;
+	isOpen_ = false;
+	for (int i = 0; i < HANDLE_COUNT; ++i)
+	{
+		if (handle_[i] != nullptr)
+			hid_close(handle_[i]);
+	}
+	Exit();
+}
+
+// 1 不阻塞； 0 阻塞；
+void MyHid::SetBlock(int b)
+{
+	for (int i = 0; i < HANDLE_COUNT; ++i)
+	{
+		if (handle_[i])
+			hid_set_nonblocking(handle_[i], b);
+	}
+}
+
+
+int MyHid::Read1(uint8_t** buf, int len)
+{
+	if (!isOpen_)
+		if (!Open())
+			return false;
+
+	if (handle_[0] == nullptr) return 0;
+	memset(buf_read1_, 0, 65);
+	int res = hid_read(handle_[0], buf_read1_, len);
+	*buf = buf_read1_;
+	return res;
+}
+
+int MyHid::Read2(uint8_t** buf, int len)
+{
+	if (!isOpen_)
+		if (!Open())
+			return false;
+
+	if (handle_[1] == nullptr) return 0;
+	memset(buf_read2_, 0, 65);
+	int res = hid_read(handle_[1], buf_read2_, len);
+	*buf = buf_read2_;
+	return res;
+}
+
+bool MyHid::Search()
+{
+	bool hasDev = false;
+	struct hid_device_info *devs;
+
+	if (!isInit_)
+	{
+		if (hid_init())
+			return false;
+		isInit_ = true;
+	}
+	if (!(devs = hid_enumerate(vid_, pid_)))
+	{// 没有搜到该设备
+		return false;
+	}
+	// 搜索到该设备，释放hid设备信息链表
+	hid_free_enumeration(devs);
+	return true;
+}
+
+void MyHid::Exit()
+{
+	if (!isExit_)
+	{
+		hid_exit();
+		isExit_ = true;
+	}	
 }
--- MyHid.h
+++ MyHid.h
@@ -9,45 +9,38 @@
 #include "hidapi.h"
 #include <stdint.h>
 
+// hid句柄数量
+#define HANDLE_COUNT 2
+
 class MyHid
 {
 public:
-	MyHid()
-	{
-		isOpen_ = false;
-		isInit_ = false;
-		vid_ = 0x0485;
-		pid_ = 0x00a8;
-		for (int i = 0; i < 5; ++i)
-		{
-			handle_[i] = nullptr;
-		}
-		devs_ = cur_dev_ = nullptr;
-	}
-	~MyHid(){}
+	MyHid();
+	MyHid(unsigned short, unsigned short);
+	~MyHid(){ Close(); }
+public:
+	void SetDevNum(unsigned short vid, unsigned short pid){ vid_ = vid; pid_ = pid; } // 设置设备号
+	void GetDevNum(unsigned short &vid, unsigned short &pid){ vid = vid_; pid = pid_; }
+	void SetBlock(int b);                  // 设置阻塞
+	bool Search();                         // 搜索指定的设备
+	bool SendCmd(uint8_t* buf, int len);   // 发送命令
+	int Read1(uint8_t** buf,int len);      // 读handle1
+	int Read2(uint8_t** buf,int len);      // 读handle2
+	void Close();                          // 关闭句柄
+private:
+	static void Exit();                    // 退出hid
+	bool Open();                           // 搜索指定的hid并打开
 public:
 	bool isOpen_;
 	bool isInit_;
 	unsigned short vid_;
 	unsigned short pid_;
-	// search ,open and sendcmd
-	bool SearchOpenSendCmd()
-	{
-		if (SearchOpen())
-		{
-			if (SendCmd1())
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	bool SearchOpen();
-	bool SendCmd1();
 private:
-	hid_device *handle_[5];             // 当前存储的设备句柄
-	struct hid_device_info *devs_;      // 设备链表
-	struct hid_device_info *cur_dev_;   // 当前使用的设备
+	static bool isExit_;
+	uint8_t buf_read1_[65];                // handle1的读缓存
+	uint8_t buf_read2_[65];                // handle2的都缓存
+	uint8_t buf_[65];                      // write buf
+	hid_device *handle_[HANDLE_COUNT];     // 当前存储的设备句柄
 };
 
 
